9a382e5eb4298747f60611fba93196219b55ff13
DeltaManiac <maxpaynered@gmail.com>
2018-02-14 22:43:30 +0530

    Added Object Prototype

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,62 @@ extern crate tcod;
 use tcod::Console;
 use tcod::console::*;
 use tcod::RootConsole;
-use tcod::colors;
+use tcod::colors::{self, Color};
 use tcod::BackgroundFlag;
 const SCREEN_WIDTH: i32 = 80;
-const SCREEN_HEIGHT:i32 = 50;
-const LIMIT_FPS:i32 = 20;
+const SCREEN_HEIGHT: i32 = 50;
+const LIMIT_FPS: i32 = 20;
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+struct Object {
+    x: i32,
+    y: i32,
+    c: char,
+    color: Color,
+}
 
-fn handle_keys(con: &mut Root, player_x: &mut i32, player_y:&mut i32) -> bool{
+impl Object {
+    pub fn new(x: i32, y: i32, c: char, color: Color) -> Self {
+        Object {
+            x: x,
+            y: y,
+            c: c,
+            color: color,
+        }
+    }
+
+    pub fn move_by(&mut self, dx: i32, dy: i32) {
+        self.x += dx;
+        self.y += dy;
+    }
+
+
+    pub fn draw(&self, con: &mut Console) {
+        con.set_default_foreground(self.color);
+        con.put_char(self.x, self.y, self.c, BackgroundFlag::None);
+    }
+
+    pub fn clear(&self, con: &mut Console) {
+        con.put_char(self.x, self.y, ' ', BackgroundFlag::None);
+    }
+}
+
+fn handle_keys(con: &mut Root, player: &mut Object) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
     let key = con.wait_for_keypress(true);
 
     match key {
-        Key {code:Enter,alt:true,..}=>{
+        Key { code: Enter, alt: true, .. } => {
             let fullscreen = con.is_fullscreen();
             con.set_fullscreen(!fullscreen);
         }
-        Key{code:Escape, ..} => return true,
-        Key{code:Up, ..} => *player_y -= 1,
-        Key{code:Down, ..} => *player_y += 1,
-        Key{code:Left, ..} => *player_x -= 1,
-        Key{code:Right, ..} => *player_x += 1,
-        _ => {},
+        Key { code: Escape, .. } => return true,
+        Key { code: Up, .. } => player.move_by(0, -1),
+        Key { code: Down, .. } => player.move_by(0, 1),
+        Key { code: Left, .. } => player.move_by(-1, 0),
+        Key { code: Right, .. } => player.move_by(1, 0),
+        _ => {}
     }
     false
 }
@@ -36,18 +70,29 @@ fn main() {
 
     let mut con_back = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let enemy = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', colors::YELLOW);
     tcod::system::set_fps(LIMIT_FPS);
+    let mut objects = [player, enemy];
 
-    let mut player_x = SCREEN_WIDTH/2;
-    let mut player_y = SCREEN_HEIGHT/2;
 
     while !con.window_closed() {
-        con_back.set_default_foreground(colors::WHITE);
-        con_back.put_char(player_x, player_y, ' ', BackgroundFlag::None);
-        let exit = handle_keys(&mut con, &mut player_x, &mut player_y);
-        con_back.put_char(player_x, player_y, '@', BackgroundFlag::None);
-        blit(&mut con_back, (0,0), (SCREEN_WIDTH, SCREEN_HEIGHT), &mut con, (0,0),1.0,1.0);
+        for object in &objects {
+            object.draw(&mut con_back);
+        }
+        blit(&mut con_back,
+             (0, 0),
+             (SCREEN_WIDTH, SCREEN_HEIGHT),
+             &mut con,
+             (0, 0),
+             1.0,
+             1.0);
         con.flush();
+        for object in &objects {
+            object.clear(&mut con_back);
+        }
+        let exit = handle_keys(&mut con, &mut objects[0]);
+        // let player = &mut objects[0];
         if exit {
             break;
         }
